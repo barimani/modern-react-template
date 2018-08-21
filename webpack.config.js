83052f18c5ec8605f873ac16532dcbf472001121
bari <moh.barimani@gmail.com>
2018-08-21 15:59:47 -0700

    webpack build config entry path fixed

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     mode: 'production',
     context: resolve(__dirname, 'src'),
     entry: {
-        app: './Root.js',
+        app: './index.js',
     },
     resolve: {
         modules: [resolve(__dirname, 'src'), 'node_modules']
